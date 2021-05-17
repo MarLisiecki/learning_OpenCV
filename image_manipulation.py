41d00c3eaf25bb14bf013cb3f3555b0660f8ffcf
M. Lisiecki <marcin.lisiecki@tutanota.com>
2021-05-17 12:29:57 +0200

    add image manipulation functions

--- image_manipulation.py
+++ image_manipulation.py
@@ -6,19 +6,43 @@ from read_image import ImageReader
 
 
 class ImageManipulation():
-    def __init__(self, vector_x: int, vector_y:int):
-        self.vector_x = vector_x
-        self.vector_y = vector_y
-        self.translation_matrix : list = np.float32([[1,0,vector_x], [0,1,vector_y]])
+    def __init__(self, image_path: str):
+        self.window_title, self.image = ImageReader.read_image_color(image_path)
 
+    def get_size(self):
+        height, width = self.image.shape[:2]
+        return height, width
 
-    @staticmethod
-    def translation(image, translation_matrix, window_title):
-        height, width = image.shape[:2]
-        translated_image = cv2.warpAffine(image,translation_matrix,(width, height))
-        ImageReader.plot_image(translated_image, window_title)
+    def translation(self, vector_x: int, vector_y: int):
+        height, width = self.get_size()
+        translation_matrix = np.float32([[1, 0, vector_x], [0, 1, vector_y]])
+        translated_image = cv2.warpAffine(self.image, translation_matrix, (width, height))
+        return translated_image, self.window_title
+
+    def rotation(self, angle: int):
+        height, width = self.get_size()
+        rotation_matrix = cv2.getRotationMatrix2D(((width - 1) / 2.0, (height - 1) / 2.0), angle, 1)
+        rotated_image = cv2.warpAffine(self.image, rotation_matrix, (width, height))
+        return rotated_image, window_title
+
+    # Linear interpolation
+    def scale_image_by_coefficient(self, x_coefficient: int, y_coefficient: int):
+        scaled_image = cv2.resize(self.image, None, fx=x_coefficient, fy=y_coefficient, interpolation=cv2.INTER_CUBIC)
+        return scaled_image, window_title
+
+    def scale_image_by_size(self, x_size: int, y_size: int):
+        height, width = self.get_size()
+        scaled_image = cv2.resize(self.image, (2 * width, 2 * height), interpolation=cv2.INTER_CUBIC)
+        return scaled_image, window_title
+
 
 if __name__ == '__main__':
-    translation_test = ImageManipulation(50, 25)
-    window_title, input_image = ImageReader.read_image_color(IMAGE_PATH)
-    ImageManipulation.translation(input_image, translation_test.translation_matrix, window_title)
+    manipulation_test = ImageManipulation(image_path=IMAGE_PATH)
+    translated_image, window_title = manipulation_test.translation(vector_x=50, vector_y=25)
+    ImageReader.plot_image(translated_image, window_title)
+    rotated_image, window_title = manipulation_test.rotation(angle=45)
+    ImageReader.plot_image(rotated_image, window_title)
+    scaled_image, window_title = manipulation_test.scale_image_by_coefficient(x_coefficient=2, y_coefficient=2)
+    ImageReader.plot_image(scaled_image, window_title)
+    scaled_image, window_title = manipulation_test.scale_image_by_coefficient(x_coefficient=2, y_coefficient=2)
+    ImageReader.plot_image(scaled_image, window_title)
